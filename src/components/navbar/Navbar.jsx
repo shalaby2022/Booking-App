6eea5c813e6bfaf358bbb5be3663f70749c17bd4
shalaby2022 <ahmedshalaby201495@gmail.com>
2022-10-13 21:44:30 +0200

    deploying the app

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.css"
-
+import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <Link to="/" className="logoLink">
+          <span className="logo">Heaven Booking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
